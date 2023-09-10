308972c2e929dec050fb3841ff3a4364309383fd
Faynot <119162922+Faynot@users.noreply.github.com>
2023-09-10 17:38:09 +0300

    Add files via upload

---
+++ res.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/style.css">
+	<title>Faynot | Resume</title>
+</head>
+<body>
+  <iframe src="res.pdf" style="width:100%; height:100vh;"></iframe>
+
+
+</body>
+</html>
